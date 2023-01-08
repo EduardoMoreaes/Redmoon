98707a0486c0c7600a9c2043a2c67e9a37572bc1
EduardoMoreaes <17eduardo05@gmail.com>
2023-01-08 01:11:36 -0300

    modified:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   server/index.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # redmoon
+
+iniciando instalando as dependências express e nodemon
+[[~~200 npm install express nodemon --save-dev]]
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+
+
+
+app.listen(8080, () =>   {
+    console.log("Aplicativo rodando na porta 8080: http://localhost:8080")
+})
